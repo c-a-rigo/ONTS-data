42915df969f3e9d970fb8eb3bb991a99d92e6137
Cezar A. Rigo <53792592+c-a-rigo@users.noreply.github.com>
2023-01-25 11:10:05 -0300

    aditional parameters

---
+++ parameters.jl
@@ -0,0 +1,6 @@
+soc0 = 0.7      # [%] de carga (0-1)
+q = 5           # [Ampere-hour] bettery cherging capacity
+v_bat = 3.6     # [V] nominal battery voltage
+bat_usage = 5   # [A] Battery capacity
+lower_bound = 0 # [%] Battery lower bound
+ef = 0.9        # [%] Battery charging/discharging efficiency 
